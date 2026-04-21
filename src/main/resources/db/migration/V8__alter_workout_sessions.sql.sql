561fd8cf8058ae71cfd73547e6654022230f4295
huaquang5320 <thomasdev@gmail.com>
2026-04-21 23:59:27 +0700

    feat: add workout domain V8 migration

---
+++ src/main/resources/db/migration/V8__alter_workout_sessions.sql.sql
@@ -0,0 +1,7 @@
+-- V8__alter_workout_sessions.sql
+ALTER TABLE workout_sessions
+    ADD COLUMN started_at TIMESTAMP,
+    ADD COLUMN ended_at TIMESTAMP;
+
+ALTER TABLE workout_sessions
+    DROP COLUMN duration_min;
